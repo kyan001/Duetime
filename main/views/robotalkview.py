bb626ca761083f09d702c66ad216486dd192e999
Kyan <ykyankai@gmail.com>
2016-09-11 22:45:59 +0800

    更新 robotalk 的代码
    Former-commit-id: fe50761e9af30a05874f088368a0e8c65ea259b0
    Former-commit-id: 1b63987fa177179389e257454e50631232c61cb9

--- main/views/robotalkview.py
+++ main/views/robotalkview.py
@@ -1,5 +1,8 @@
 import json
 import urllib.request
+import re
+import datetime
+
 from django.shortcuts import render_to_response
 from django.http import JsonResponse
 
@@ -12,38 +15,80 @@ def robotalkIndex(request):
 def robotalkGetresponse(request):  # AJAX
     """Get input and take back request via AJAX"""
     userinput = request.GET.get('txt')
+    from_ = request.GET.get('from')
     if not userinput:
         return JsonResponse({'error': 'userinput is empty'})
 
-    def getResponse(url, param):
-        if param:
-            param = urllib.parse.urlencode(param)
-        u = urllib.request.urlopen("{u}?{p}".format(u=url, p=param))
+    def getFullurl(robo):
+        if not (robo and robo.get('param') and robo.get('url')):
+            return None
+        param = urllib.parse.urlencode(robo.get('param'))
+        fullurl = "{u}?{p}".format(u=robo.get('url'), p=param)
+        return fullurl
+
+    def getResponse(robo):
+        fullurl = getFullurl(robo)
+        u = urllib.request.urlopen(fullurl)
         u_resp = u.read()
         if not u_resp:
             return None
-        return u_resp.decode()
-    robo1 = 'http://www.niurenqushi.com/app/simsimi/ajax.aspx'
-    robo1_param = {'txt': userinput}
-    robo2 = 'http://api.qingyunke.com/api.php'
-    robo2_param = {
-        'key': 'free',
-        'appid': 0,
-        'msg': userinput,
-    }
-    robo1_says = getResponse(robo1, robo1_param)
-    robo2_resp = getResponse(robo2, robo2_param)
-    robo2_says = json.loads(robo2_resp).get('content').replace('{br}', '<br/>')
-    result = {
-        'result1': {
-            'txt': robo1_says,
-            'url': robo1,
+        resp_str = u_resp.decode()
+        return robo.get('getContent')(resp_str)
+
+    def addToResult(robo, result):
+        key = robo.get('from')
+        time_now = datetime.datetime.now()
+        value = {
+            'txt': getResponse(robo),
+            'fullurl': getFullurl(robo),
+        }
+        time_rtt = (datetime.datetime.now() - time_now).microseconds / 1000
+        value['rtt'] = int(time_rtt)  # milliseconds
+        result['result'][key] = value
+        return True
+
+    def extractFeifei(content: str):
+        """从 菲菲 的返回字符串中获得真正的内容"""
+        if not content:
+            return None
+        json_obj = json.loads(content)
+        content = json_obj.get('content').replace('{br}', '<br/>')
+        content = re.sub(r'{face:[0-9]+}', '', content)
+        return content
+
+    def extractSimsimi(content: str):
+        """从 Simsimi 的返回字符串中获得真正的内容"""
+        if not content:
+            return None
+        content = content.replace('\n', '<br/>')
+        return content
+
+    # robos list(dictionary)
+    robos = {
+        'simsimi': {
             'from': 'simsimi',
+            'url': 'http://www.xiaodoubi.com/simsimiapi.php',
+            'param': {'msg': userinput},
+            'getContent': extractSimsimi,
         },
-        'result2': {
-            'txt': robo2_says,
-            'url': robo2,
+        'feifei': {
             'from': 'feifei',
+            'url': 'http://api.qingyunke.com/api.php',
+            'param': {
+                'key': 'free',
+                'appid': 0,
+                'msg': userinput,
+            },
+            'getContent': extractFeifei,
         },
     }
+    # get results
+    result = {'result': {}}
+    if from_:
+        robo = robos.get(from_)
+        addToResult(robo, result)
+    else:
+        for i in robos:
+            addToResult(robos.get(i), result)
+    # render
     return JsonResponse(result)
