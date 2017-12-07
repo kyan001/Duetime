f24a1b315aa8e7d4cc9f2919bdbf693560022b23
Kyan <ykyankai@gmail.com>
2017-12-07 11:36:10 +0800

    add BaseModel and BaseManager

--- main/models.py
+++ main/models.py
@@ -1,7 +1,35 @@
 from django.db import models
 
 
-class Notiecard(models.Model):
+class BaseManager(models.Manager):
+    def get_or_none(self, *args, **kwargs):
+        try:
+            return self.get(*args, **kwargs)
+        except self.model.DoesNotExist:
+            return None
+
+    def get_or_404(self, *args, **kwargs):
+        try:
+            return self.get(*args, **kwargs)
+        except self.model.DoesNotExist:
+            raise Http404('您查找的 {t} 并不存在。（查询参数 {a} {k}）'.format(t=self.model.__name__, a=args or '', k=kwargs or ''))
+
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    objects = BaseManager()
+
+    class Meta:
+        abstract = True
+
+    def toArray(self):
+        self.created = self.created.isoformat(' ')
+        self.modified = self.modified.isoformat(' ')
+        return model_to_dict(self)
+
+
+class Notiecard(BaseModel):
     title = models.CharField(max_length=30)
     kcol = models.CharField(max_length=30)
     vcol = models.CharField(max_length=30)
