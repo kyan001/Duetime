080b343611419fdd41132ab8f907e440c431701e
zyjyuyan <zyjyuyan@163.com>
2017-12-07 12:54:33 +0800

    add notie list page

--- main/views/zyj.py
+++ main/views/zyj.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
 
+from main.models import Notiecard, Notieitem
+
+
 def zyjNotie(request):
 	""" 以卡片形式记录日常
 	"""
 	# username = request.GET['zyj']
 	# feeType = request.GET['Water']
+	cards = Notiecard.objects.all()
 	# render
 	context = {
-		'date': '日期',
-		'price': '元',
-		'attri_detail': {
-				'2016-11-01': '15',
-				'2016-11-03': '2',
-				'2016-11-06': '6',
-		},
-
+		'cards': cards
 	}
 	return render(request,'zyj/notie.html',context)
