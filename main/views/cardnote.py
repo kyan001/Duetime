0af08a60053a20f7bb67b0df5841bdde315be1e6
Kyan <ykyankai@gmail.com>
2017-12-11 18:00:44 +0800

    done with detail page, and update cardnote/update

--- main/views/cardnote.py
+++ main/views/cardnote.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import Http404
@@ -43,48 +45,62 @@ def cardnoteAddcard(request):
 
 
 def cardnoteDeletecard(request):
-	"""删除一个卡片"""
-	cardnotecardid = request.GET.get('id') or 0
-	if not cardnotecardid:
-		raise Http404("Id should not be empty")
-	card = CardnoteCard.objects.get_or_404(id=int(cardnotecardid))
-	items = card.cardnoteitems
-	with transaction.atomic():
-		messages.success(request, "卡片《{card.title}》已删除".format(card=card))
-		card.delete()
-		items.delete()
-	return redirect('/cardnote/list')
-
-
-	cardnotecardid = request.POST.get('id') or 0
-	if not cardnotecardid:
-		raise Http404("Id should not be empty")
-	title = request.POST.get('title')
-def cardnoteUpdate(request):
-    """Update Card and Items"""
-    kcol = request.POST.get('kcol')
-    vcol = request.POST.get('vcol')
-    if not title:
-        raise Http404('Title cannot be empty')
+    """删除一个卡片"""
+    cardnotecardid = request.GET.get('id') or 0
+    if not cardnotecardid:
+        raise Http404("Id should not be empty")
     card = CardnoteCard.objects.get_or_404(id=int(cardnotecardid))
-    card.title = title
-    card.kcol = kcol
-    card.vcol = vcol
-    card.save()
-    # render
-    messages.success(request, '卡片《{card.title}》已更新'.format(card=card))
+    items = card.cardnoteitems
+    with transaction.atomic():
+        messages.success(request, "卡片《{card.title}》已删除".format(card=card))
+        card.delete()
+        items.delete()
     return redirect('/cardnote/list')
 
 
+def cardnoteUpdate(request):
+    """Update Card and Items"""
+    # get inputs
+    cardnotecardid = request.POST.get('id') or 0
+    title = request.POST.get('title')
+    kcol = request.POST.get('kcol')
+    vcol = request.POST.get('vcol')
+    new_items_in_json = request.POST.get('newitems')
+    # inputs validation
+    if not cardnotecardid:
+        raise Http404("Id should not be empty")
+    if not title:
+        raise Http404('Title cannot be empty')
+    # get card and items
+    card = CardnoteCard.objects.get_or_404(id=int(cardnotecardid))
+    new_items = json.loads(new_items_in_json) if new_items_in_json else []
+    # update and save and delete
+    with transaction.atomic():
+        # card part
+        card.title = title
+        card.kcol = kcol
+        card.vcol = vcol
+        card.save()
+        # items part
+        card.cardnoteitems.delete()
+        for new_item in new_items:
+            if new_item.get("kword") or new_item.get("val"):
+                tmp_itm = CardnoteItem(cardnotecardid=card.id)
+                tmp_itm.kword = new_item.get("kword")
+                tmp_itm.val = new_item.get("val")
+                tmp_itm.save()
+    # render
+    messages.success(request, '卡片《{card.title}》已更新'.format(card=card))
+    return redirect('/cardnote/detail?id={}'.format(card.id))
+
+
 def cardnoteDetail(request):
-	"""卡片详情页"""
-	cardnotecardid = request.GET.get('id') or 0
-	if not cardnotecardid:
-		raise Http404("Id should not be empty")
-	card = CardnoteCard.objects.get_or_404(id=int(cardnotecardid))
-	context = {
-		'card': card
-	}
-	return render(request, 'cardnote/detail.html', context)
-
-
+    """卡片详情页"""
+    cardnotecardid = request.GET.get('id') or 0
+    if not cardnotecardid:
+        raise Http404("Id should not be empty")
+    card = CardnoteCard.objects.get_or_404(id=int(cardnotecardid))
+    context = {
+        'card': card
+    }
+    return render(request, 'cardnote/detail.html', context)
